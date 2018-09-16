772e1bb9c4fc9519fd39ad226d72fed99917d102
marodere <marodere@up4k.org>
2018-09-16 05:22:36 +0300

    header update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Module: default
-# Author: Roman V. M.
-# Created on: 28.11.2014
+# Author: marodere
+# Created on: 16.09.2018
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import sys
